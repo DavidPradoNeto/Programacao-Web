d1bc0ace36bde6ed2424b03b9fb74afc9f171fb1
Amanda <amandadamasceno@alunos.utfpr.edu.br>
2022-05-08 20:19:14 -0300

    Adicionado código de login

--- Projeto 2/scripts.js
+++ Projeto 2/scripts.js
@@ -8,16 +8,27 @@ function getSession(){
 }
 
 function loadUser(){
+    mostraLoader(true);
     setTimeout(() => {
         let { email, first_name, last_name,  avatar } = JSON.parse(sessionStorage.getItem('userData'))
         document.querySelector('#userImg').src = avatar;
         document.querySelector('#userDados').innerHTML = first_name+" "+last_name+"<br>"+email;
+        mostraLoader(false);
     }, "1000")
 }
 
 function logout(){
     sessionStorage.clear();
     location.reload();
+}
+
+function mostraLoader(opt) {
+    var loader = document.querySelector('.loader');
+    if(opt)
+        loader.style.visibility = "visible";
+    else
+        loader.style.visibility = "hidden";
+
 }
 
 function validaLogin(email, senha) {
@@ -41,6 +52,28 @@ function validaLogin(email, senha) {
         return true;
     else
         return false;
+}
+
+function entrarGoogle(){
+    document.querySelector('.pagina-inicial').style = "width: 0; height: 0; visibility: hidden;";
+    getUser();
+    document.querySelector('.pagina-logado').style = "width: 100%; height: 100%; visibility: visible;";
+    loadUser();
+}
+
+function getUser(){
+    var request = new XMLHttpRequest();
+
+            request.open('GET', 'https://reqres.in/api/users', true);
+
+            request.onreadystatechange = function () {
+                if (request.readyState === 4 && request.status === 200){
+                    var response = JSON.parse(request.responseText),
+                        user = response.data[Math.floor(Math.random() * 6)];    // recebe um usuario aleatorio da API
+                        sessionStorage.setItem('userData', JSON.stringify(user));  // armazena esse usuario no localStorage para manter o login
+                }
+            }
+            request.send();
 }
 
 function buscar(pesquisa){
